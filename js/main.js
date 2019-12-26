79d99d83f88fa886b6df0be67781641f358a12f8
ijelechimaobi <45017704+ijelechimaobi@users.noreply.github.com>
2019-12-27 00:50:39 +0100

    Interfaces and layouts

---
+++ js/main.js
@@ -0,0 +1,46 @@
+activateBtn();
+
+//---- Start Chat Interface
+
+$('#contBtn').on('click', function() {
+    $(this).removeClass('activeBtn');
+    $('.intro_cont').hide(500);
+    startChatBot();
+});
+
+//---- Play Sound on Button Click
+
+const clickSound = new Audio('audio/Button-click-sound.mp3');
+
+startSound();
+
+function startSound() {
+    $('.click_sound').click(e => clickSound.play());
+}
+
+function startChatBot() {
+    $('body').addClass('beginChat');
+}
+
+function activateBtn() {
+    $(function () {
+        $('#nameInput').keyup(function () {
+            if ($(this).val() == '') {
+                $('#contBtn').removeClass('activeBtn');
+            }
+            else {
+                $('#contBtn').addClass('activeBtn');
+            }
+        });
+    });
+}
+
+$(function () {
+    $('#messageBox').keyup(function () {
+        if ($(this).val() == '') {
+            $('#sendBtn').removeClass('activeSend');
+        } else {
+            $('#sendBtn').addClass('activeSend');
+        }
+    });
+});
